b93dadd8a814f46a9ee39398a794383b7c9410a3
Gurveer <gurveer7@hotmail.com>
2025-02-04 17:40:32 -0500

    dummy data for custom player

--- milestone-1/createPlayerQueries.sql
+++ milestone-1/createPlayerQueries.sql
@@ -1,7 +1,8 @@
 -- CREATE CUSTOM PLAYER: (Players Relation name is TENTATIVE)
-INSERT INTO Players (player, age, team, pos...)
+INSERT INTO Players (Player, Age, Team, Pos, G, MP, FG, 3P, 3PA, eFG%, FT, FTA, FT%, ORB, DRB, TRB, AST, STL, BLK, TOV, PF, PTS)
+VALUES ('luka laker', 25, 'LAC', 'PG', 44, 22.5, 10, 40, 70, 67, 55, 30, 50, 60, 21, 42, 63, 11, 2, 2, 4, 15, 200)
 -- FIX TO INCLUDE THE CORRECT ATTRIBUTES WHEN CHOSEN^
-
+-
 
 DELETE FROM Players
 WHERE Players.name = '' 
