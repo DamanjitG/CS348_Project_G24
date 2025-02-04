19a9579d1055cc92fca71321224dd05ad4f5596e
Gurveer <gurveer7@hotmail.com>
2025-02-04 17:20:41 -0500

    temp player creation

---
+++ milestone-1/createPlayerQueries.sql
@@ -0,0 +1,9 @@
+-- CREATE CUSTOM PLAYER: (Players Relation name is TENTATIVE)
+INSERT INTO Players (player, age, team, pos...)
+-- FIX TO INCLUDE THE CORRECT ATTRIBUTES WHEN CHOSEN^
+
+
+DELETE FROM Players
+WHERE Players.name = '' 
+--should probably show the users a list of players 
+-- and get the metadata that way (from FE)?
