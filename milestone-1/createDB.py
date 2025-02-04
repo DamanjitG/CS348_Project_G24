2f85b1fd9c483c8b48b66c4900ac948cf05ca38c
Gurveer <gurveer7@hotmail.com>
2025-02-04 16:53:47 -0500

    adding user table schema into script, with inserts and print file

---
+++ milestone-1/createDB.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+conn = sqlite3.connect('m1.db')
+cursor = conn.cursor()
+
+deleteQuery = "DROP TABLE IF EXISTS TESTDB"
+cursor.execute(deleteQuery)
+
+createTestDB = """ CREATE TABLE TESTDB (
+                    name VARCHAR(255) NOT NULL,
+                    age INT NOT NULL,
+                    dob DATE NOT NULL,
+                    email VARCHAR(255)
+                );
+              """
+# Create Test-DB (M0)
+cursor.execute(createTestDB)
+
+# Insert Values into Test-DB 
+insertQuery1 = """ INSERT INTO TESTDB VALUES(
+                    'Alice Andrews',
+                    25,
+                    '2000-01-01',
+                    'alice.andrews@test.com'
+                );
+               """
+insertQuery2 = """ INSERT INTO TESTDB VALUES(
+                    'Bobby Bell',
+                    37,
+                    '1997-09-07',
+                    'bobby.bell@test.org'
+                );
+               """
+
+cursor.execute(insertQuery1)
+cursor.execute(insertQuery2)
+
+cursor.execute("DROP TABLE IF EXISTS Users")
+createUsersTable = """ CREATE TABLE Users (
+                        uid INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username VARCHAR(20) NOT NULL,
+                        password VARCHAR(20) NOT NULL
+                );
+            """
+cursor.execute(createUsersTable)
+
+insertUsers = """
+        INSERT INTO Users (uid, username, password) VALUES
+        (1, 'user1', 'user1pass'),
+        (2, 'user2', 'user2pass');
+"""
+
+cursor.execute(insertUsers)
+
+conn.commit()
+
+# Close the cursor
+cursor.close()
+conn.close()
