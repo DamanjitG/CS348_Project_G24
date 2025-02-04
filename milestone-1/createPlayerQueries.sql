a7c6000e7d1a8fdce0472f34919286d1601f2113
Gurveer <gurveer7@hotmail.com>
2025-02-04 17:51:04 -0500

    included uid relation in custom player query

--- milestone-1/createPlayerQueries.sql
+++ milestone-1/createPlayerQueries.sql
@@ -1,6 +1,7 @@
 -- CREATE CUSTOM PLAYER: (Players Relation name is TENTATIVE)
-INSERT INTO Players (Player, Age, Team, Pos, G, MP, FG, 3P, 3PA, eFG%, FT, FTA, FT%, ORB, DRB, TRB, AST, STL, BLK, TOV, PF, PTS)
-VALUES ('luka laker', 25, 'LAC', 'PG', 44, 22.5, 10, 40, 70, 67, 55, 30, 50, 60, 21, 42, 63, 11, 2, 2, 4, 15, 200)
+-- uid is a FORIGN KEY REFERENCING A User
+INSERT INTO Players (uid, Player, Age, Team, Pos, G, MP, FG, 3P, 3PA, eFG%, FT, FTA, FT%, ORB, DRB, TRB, AST, STL, BLK, TOV, PF, PTS)
+VALUES (1, 'luka laker', 25, 'LAC', 'PG', 44, 22.5, 10, 40, 70, 67, 55, 30, 50, 60, 21, 42, 63, 11, 2, 2, 4, 15, 200)
 -- FIX TO INCLUDE THE CORRECT ATTRIBUTES WHEN CHOSEN^
 -
 
