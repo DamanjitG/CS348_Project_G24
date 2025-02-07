2dd069dc7b423ed69583024e77dee74d92cb703f
Gurveer <gurveer7@hotmail.com>
2025-02-07 13:09:48 -0500

    simple feature 2

--- milestone-1/createPlayerQueries.sql
+++ milestone-1/createPlayerQueries.sql
@@ -1,13 +1,64 @@
--- CREATE CUSTOM PLAYER: (Players Relation name is TENTATIVE)
--- uid is a FORIGN KEY REFERENCING A User
-INSERT INTO Players (Player, Age, Team, Pos, G, MP, FG, 3P, 3PA, eFG%, FT, FTA, FT%, ORB, DRB, TRB, AST, STL, BLK, TOV, PF, PTS, uid)
-VALUES ('luka laker', 25, 'LAC', 'PG', 44, 22.5, 10, 40, 70, 67, 55, 30, 50, 60, 21, 42, 63, 11, 2, 2, 4, 15, 200, 1)
--- FIX TO INCLUDE THE CORRECT ATTRIBUTES WHEN CHOSEN^
--
+-- Template
+INSERT INTO players (
+    pid, name, age, team, pos, g, mp, fg, fga, fgp,
+    threept, threepta, efgp, ft, fta, ftp,
+    orb, drb, trb, ast, stl, blk, tov, pf, pts, creator
+)
+VALUES (
+    $pid, $name, $age, $team, $pos, $g, $mp, $fg, $fga, $fgp,
+    $threept, $threepta, $efgp, $ft, $fta, $ftp,
+    $orb, $drb, $trb, $ast, $stl, $blk, $tov, $pf, $pts, $creator
+);
 
-DELETE FROM Players
-WHERE Players.name = '' AND Players.uid IS NOT NULL 
---should probably show the users a list of players 
--- and get the metadata that way (from FE)?
--- How do we join pid with uid? 
--- NOT FINISHED!!
+-- Inserts a custom player to the DB
+INSERT INTO players (
+    pid, name, age, team, pos, g, mp, fg, fga, fgp,
+    threept, threepta, efgp, ft, fta, ftp,
+    orb, drb, trb, ast, stl, blk, tov, pf, pts, creator
+)
+VALUES (
+    1000,                   
+    'Luka Lakers',          
+    25,                     
+    'LAL',                  
+    'SG',                   
+    42,                     
+    42.5,                   
+    15,                     
+    20,                     
+    75.0,                   
+    4,                      
+    6,                      
+    80.0,                   
+    8,                     
+    10,                     
+    80.0,                   
+    4,
+    5,
+    9, 
+    7,         
+    2,                  
+    1,
+    3,                     
+    2,                     
+    42,                     
+    'DataBaseLover'         
+);
+-- Returns all custom players
+SELECT * 
+FROM players
+WHERE creator IS NOT NULL;
+
+
+-- Returns the custom player of a certain position
+SELECT * 
+FROM players
+WHERE creator IS NOT NULL
+AND pos = $pos
+
+--Returns custom players with the highest point scorers at the top
+SELECT * 
+FROM players
+WHERE creator IS NOT NULL
+ORDER BY pts DESC
+
