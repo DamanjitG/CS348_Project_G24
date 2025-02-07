fd7f3c4fa66ba94ca39328f1413d4f1daccae8b5
aryan-rastogi <75449029+aryan-rastogi@users.noreply.github.com>
2025-02-07 12:51:43 -0500

    added sample queries, output

--- milestone-1/R9-playertable/select_players.sql
+++ milestone-1/R9-playertable/select_players.sql
@@ -1,4 +1,19 @@
 select name, team, age, pos, g, mp, fg, threept, ft, trb, ast, stl, blk, tov, pts, (pts + trb + ast + stl + blk - tov) as fantasy from players as p
 where p.pos = 'PG'
 and name like '%Don%'
-order by fantasy desc
+order by fantasy desc;
+
+select name, team, age, pos, g, mp, fg, threept, ft, trb, ast, stl, blk, tov, pts, (pts + trb + ast + stl + blk - tov) as fantasy from players as p
+where p.pos = 'PG'
+and name like '%Don%'
+order by tov asc;
+
+select name, team, age, pos, g, mp, fg, threept, ft, trb, ast, stl, blk, tov, pts, (pts + trb + ast + stl + blk - tov) as fantasy from players as p
+where p.pos = 'C'
+and name like '%Nik%'
+order by trb desc;
+
+select name, team, age, pos, g, mp, fg, threept, ft, trb, ast, stl, blk, tov, pts, (pts + trb + ast + stl + blk - tov) as fantasy from players as p
+where p.pos = 'PF'
+and name like '%%'
+order by ast desc;
