bd261f1b02a1703155796b332390bede0d684d0e
aryan-rastogi <75449029+aryan-rastogi@users.noreply.github.com>
2025-03-25 14:36:51 -0400

    added trigger, additional checks

--- milestone-3/sql/R9-PlayerTable/test-sample.sql
+++ milestone-3/sql/R9-PlayerTable/test-sample.sql
@@ -1,19 +1,19 @@
-select name, team, age, pos, g, mp, fg, threept, trb, ast, stl, blk, tov, pts, fantasy from players as p
+select name, team, age, pos, g, mp, fg, threept, ft, trb, ast, stl, blk, tov, pts, fantasy from players as p
 where p.pos = 'PG'
 and name like '%Don%'
 order by fantasy desc;
 
-select name, team, age, pos, g, mp, fg, threept, trb, ast, stl, blk, tov, pts, fantasy from players as p
+select name, team, age, pos, g, mp, fg, threept, ft, trb, ast, stl, blk, tov, pts, fantasy from players as p
 where p.pos = 'PG'
 and name like '%Don%'
 order by tov asc;
 
-select name, team, age, pos, g, mp, fg, threept, trb, ast, stl, blk, tov, pts, fantasy from players as p
+select name, team, age, pos, g, mp, fg, threept, ft, trb, ast, stl, blk, tov, pts, fantasy from players as p
 where p.pos = 'C'
 and name like '%Nik%'
 order by trb desc;
 
-select name, team, age, pos, g, mp, fg, threept, trb, ast, stl, blk, tov, pts, fantasy from players as p
+select name, team, age, pos, g, mp, fg, threept, ft, trb, ast, stl, blk, tov, pts, fantasy from players as p
 where p.pos = 'PF'
 and name like '%%'
 order by ast desc;
